501a11999f90c991953437978e03869541a195b6
KokWeiKang <iamnext6926@gmail.com>
2024-05-11 06:03:46 +0800

    Update _layout.tsx

--- src/app/(user)/billing/_layout.tsx
+++ src/app/(user)/billing/_layout.tsx
@@ -1,10 +1,12 @@
 import {Stack} from 'expo-router'
+import Colors from '@/constants/Colors';
 
 export default function AnnouncementStack() {
     return(
         <Stack>
             {/* Header Name*/}
-            <Stack.Screen name="billing" options={{title:'Billing'}}/>
+            <Stack.Screen name="billing" options={{title:'Billing', headerTintColor: '#FFFFFF', headerStyle: {backgroundColor: Colors.light.tint,// Change this line with your desired color
+                },}}/>
         </Stack>
     )
 }
