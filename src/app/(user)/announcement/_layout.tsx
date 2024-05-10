13e260b77dfedf000e6fa04b678c0b187dab14cb
KokWeiKang <iamnext6926@gmail.com>
2024-05-11 04:24:03 +0800

    Update _layout.tsx

--- src/app/(user)/announcement/_layout.tsx
+++ src/app/(user)/announcement/_layout.tsx
@@ -1,10 +1,19 @@
-import {Stack} from 'expo-router'
+import { Stack } from 'expo-router';
 
 export default function AnnouncementStack() {
-    return(
+    return (
         <Stack>
             {/* Header Name*/}
-            <Stack.Screen name="announcement" options={{title:'Announcement'}}/>
+            <Stack.Screen 
+                name="announcement" 
+                options={{
+                    title: 'Announcement',
+                    headerStyle: {
+                        backgroundColor: '#2EAED1', // Change this to the color you want
+                    },
+                    headerTintColor: 'white', // Change this to the color you want for the text
+                }}
+            />
         </Stack>
-    )
+    );
 }
