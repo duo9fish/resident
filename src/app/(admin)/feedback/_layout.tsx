1e645079aff603746860d7eb19bab56b940e8166
KokWeiKang <iamnext6926@gmail.com>
2024-05-11 04:13:23 +0800

    Update _layout.tsx

--- src/app/(admin)/feedback/_layout.tsx
+++ src/app/(admin)/feedback/_layout.tsx
@@ -4,7 +4,7 @@ export default function FeedbackStack() {
   return (
     <Stack>
       {/* <Stack.Screen name="index" options={{ title: 'Orders' }} /> */}
-      <Stack.Screen name="feedback" options={{ headerShown: true, title:'Feedback'}} />
+      <Stack.Screen name="feedback" options={{ headerShown: true, title:'Feedback', headerStyle: { backgroundColor: '#2EAED1',}}} />
     </Stack>
   );
 }
