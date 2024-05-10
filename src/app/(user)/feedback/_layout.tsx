e914d0d1f49dfa053810fb13726379f5b2a5a20e
KokWeiKang <iamnext6926@gmail.com>
2024-05-11 04:26:11 +0800

    Update _layout.tsx

--- src/app/(user)/feedback/_layout.tsx
+++ src/app/(user)/feedback/_layout.tsx
@@ -4,7 +4,17 @@ export default function MenuStack() {
   return (
     <Stack>
       {/* <Stack.Screen name="index" options={{ title: 'Orders' }} /> */}
-      <Stack.Screen name="list" options={{ headerShown: true, title:'Feedback'}} />
+      <Stack.Screen
+        name="list"
+        options={{
+          headerShown: true,
+          title: 'Feedback',
+          headerStyle: {
+            backgroundColor: '#2EAED1', // Change the header background color
+          },
+          headerTintColor: '#FFFFFF', // Change the title (word) color
+        }}
+      />
     </Stack>
   );
 }
